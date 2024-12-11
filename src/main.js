c1de88d0e37272d30fed48b67f3935f999e87325
NizarNaser <nizarnaser17@gmail.com>
2024-12-11 23:40:34 +0100

    Fifth commit

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter} from 'react-router-dom'
 import StoreContextProvider from './context/StoreContext.jsx'
-import './i18n';
+import './i18n.jsx';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
