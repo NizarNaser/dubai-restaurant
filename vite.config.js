87184c84200abc6718e51810af32b3cc5168fd69
NizarNaser <nizarnaser17@gmail.com>
2024-12-11 23:49:19 +0100

    six commit

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  
-    esbuild: {
-      jsxInject: `import React from 'react'`,
-    },
-  
   plugins: [react()],
-  base: '/Dubai-restaurant/' 
-})
+  base: '/dubai-restaurant/', // قم بتغيير هذا إلى اسم مستودع GitHub الخاص بك
+});
+
 
 
