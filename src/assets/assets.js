ebcbcba1a18d762bcdedb9910a8d1dd4ca4531fd
NizarNaser <nizarnaser17@gmail.com>
2024-12-11 22:38:52 +0100

    вке

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -6,7 +6,14 @@ import Shakshuka_classic_cat from './menu/BREAKFAST/Shakshuka_classic_cat.webp'
 import Seafood_Salad_cat from './menu/SALAD/Seafood_Salad_cat.webp'
 import Lentil_Cream_Soup_cat from './menu/SOUP/Lentil_Cream_Soup_cat.webp'
 import Classic_Hummus_cat from './menu/APPETIZERS/Classic_Hummus_cat.webp'
-
+import Mixed_Meat_Shashlik_Platter_1000_cat from './menu/GRILL/Mixed_Meat_Shashlik_Platter_1000_cat.webp'
+import HOT_APPETIZERS_cat from './menu/HOT APPETIZERS/HOT_APPETIZERS_cat.webp'
+import MAIN_COURSE_cat from './menu/MAIN COURSE/MAIN_COURSE_cat.webp'
+import BAKING_cat from './menu/BAKING/BAKING_cat.webp'
+import PIZZA_cat from './menu/PIZZA/PIZZA_cat.webp'
+import SANDWICHES_cat from './menu/SANDWICHES/SANDWICHES_cat.webp'
+import SIDES_cat from './menu/SIDES/SIDES_cat.webp'
+import DESSERTS_cat from './menu/DESSERTS/DESSERTS_cat.webp'
 
 //BREAKFAST
 import Shakshuka_classic from './menu/BREAKFAST/Shakshuka_classic.webp'
@@ -48,41 +55,58 @@ import Basturma from './menu/APPETIZERS/Basturma.webp'
 import Veal_Tartar from './menu/APPETIZERS/Veal_Tartar.webp'
 import Salmon_Tartar from './menu/APPETIZERS/Salmon_Tartar.webp'
 //MAIN COURSE
-import Dolma from './menu/BREAKFAST/Shakshuka_classic.webp'
-import Capsies_with_chicken from './menu/BREAKFAST/Shakshuka_classic.webp'
-import Fajita from './menu/BREAKFAST/Shakshuka_classic.webp'
-import Philadelphia_Veal from './menu/BREAKFAST/Shakshuka_classic.webp'
-import Bolognese from './menu/BREAKFAST/Shakshuka_classic.webp'
-import Carbonara_chicken from './menu/BREAKFAST/Shakshuka_classic.webp'
-import Veal_Carbonara from './menu/BREAKFAST/Shakshuka_classic.webp'
-import Salmon_Pasta from './menu/BREAKFAST/Shakshuka_classic.webp'
-import Dumplings from './menu/BREAKFAST/Shakshuka_classic.webp'
+import Dolma from './menu/MAIN COURSE/Dolma.webp'
+import Capsies_with_chicken from './menu/MAIN COURSE/Capsies_with_chicken.webp'
+import Fajita from './menu/MAIN COURSE/Fajita.webp'
+import Philadelphia_Veal from './menu/MAIN COURSE/Philadelphia_Veal.webp'
+import Bolognese from './menu/MAIN COURSE/Bolognese.webp'
+import Carbonara_chicken from './menu/MAIN COURSE/Carbonara_chicken.webp'
+import Veal_Carbonara from './menu/MAIN COURSE/Veal_Carbonara.webp'
+import Salmon_Pasta from './menu/MAIN COURSE/Salmon_Pasta.webp'
+import Dumplings from './menu/MAIN COURSE/Dumplings.webp'
 //GRILL
-import Lamb_shashlik from './menu/BREAKFAST/Shakshuka_classic.webp'
-import Veal_Shashlik from './menu/BREAKFAST/Shakshuka_classic.webp'
-import Chicken_Shashlik from './menu/BREAKFAST/Shakshuka_classic.webp'
-import Lamb_rack from './menu/BREAKFAST/Shakshuka_classic.webp'
-import Mixed_Meat_Shashlik_Platter_1000 from './menu/BREAKFAST/Shakshuka_classic.webp'
-import Mixed_Meat_Shashlik_Platter_500 from './menu/BREAKFAST/Shakshuka_classic.webp'
-import Lamb_kebab from './menu/BREAKFAST/Shakshuka_classic.webp'
-import Veal_kebab from './menu/BREAKFAST/Shakshuka_classic.webp'
-import Chicken_Kebab from './menu/BREAKFAST/Shakshuka_classic.webp'
-import Grilled_Kebab_Mix_1000 from './menu/BREAKFAST/Shakshuka_classic.webp'
-import Grilled_Kebab_Mix_500 from './menu/BREAKFAST/Shakshuka_classic.webp'
-import Chicken_Wings from './menu/BREAKFAST/Shakshuka_classic.webp'
-import Grilled_Vegetables_300 from './menu/BREAKFAST/Shakshuka_classic.webp'
-import Grilled_Vegetables_150 from './menu/BREAKFAST/Shakshuka_classic.webp'
-import Grilled_Potatos from './menu/BREAKFAST/Shakshuka_classic.webp'
-import RibEye_steak from './menu/BREAKFAST/Shakshuka_classic.webp'
-import Tbone_steak from './menu/BREAKFAST/Shakshuka_classic.webp'
-import Chicken_steak from './menu/BREAKFAST/Shakshuka_classic.webp'
-import Lamb_rack_set from './menu/BREAKFAST/Shakshuka_classic.webp'
-import Seafood_Saute from './menu/BREAKFAST/Shakshuka_classic.webp'
-import Salmon_steak_with_side_dish from './menu/BREAKFAST/Shakshuka_classic.webp'
-import Dorado_with_a_side_dish from './menu/BREAKFAST/Shakshuka_classic.webp'
-import Mackerel from './menu/BREAKFAST/Shakshuka_classic.webp'
-import Trout_with_a_side_dish from './menu/BREAKFAST/Shakshuka_classic.webp'
-import Royal_Shrimp from './menu/BREAKFAST/Shakshuka_classic.webp'
+import Lamb_shashlik from './menu/GRILL/Lamb_shashlik.webp'
+import Veal_Shashlik from './menu/GRILL/Veal_Shashlik.webp'
+import Chicken_Shashlik from './menu/GRILL/Chicken_Shashlik.webp'
+import Lamb_rack from './menu/GRILL/Lamb_rack.webp'
+import Mixed_Meat_Shashlik_Platter_1000 from './menu/GRILL/Mixed_Meat_Shashlik_Platter_1000.webp'
+import Lamb_kebab from './menu/GRILL/Lamb_kebab.webp'
+import Chicken_Kebab from './menu/GRILL/Chicken_Kebab.webp'
+import Grilled_Kebab_Mix_1000 from './menu/GRILL/Grilled_Kebab_Mix_1000.webp'
+import Chicken_Wings from './menu/GRILL/Chicken_Wings.webp'
+import Grilled_Vegetables_300 from './menu/GRILL/Grilled_Vegetables_300.webp'
+import Grilled_Potatos from './menu/GRILL/Grilled_Potatos.webp'
+import RibEye_steak from './menu/GRILL/RibEye_steak.webp'
+import Tbone_steak from './menu/GRILL/Tbone_steak.webp'
+import Chicken_steak from './menu/GRILL/Chicken_steak.webp'
+import Lamb_rack_set from './menu/GRILL/Lamb_rack_set.webp'
+import Seafood_Saute from './menu/GRILL/Seafood_Saute.webp'
+import Salmon_steak_with_side_dish from './menu/GRILL/Salmon_steak_with_side_dish.webp'
+import Dorado_with_a_side_dish from './menu/GRILL/Dorado_with_a_side_dish.webp'
+import Mackerel from './menu/GRILL/Mackerel.webp'
+import Trout_with_a_side_dish from './menu/GRILL/Trout_with_a_side_dish.webp'
+import Royal_Shrimp from './menu/GRILL/Royal_Shrimp.webp'
+import Hummus_with_veal from './menu/HOT APPETIZERS/Hummus_with_veal.webp'
+import Fried_Kebbe from './menu/HOT APPETIZERS/Fried_Kebbe.webp'
+import Chicken_liver_with_onions from './menu/HOT APPETIZERS/Chicken_liver_with_onions.webp'
+//BAKING
+import Pidet_with_chicken from './menu/BAKING/Pidet_with_chicken.webp'
+import Pidet_with_meat_and_cheese from './menu/BAKING/Pidet_with_meat_and_cheese.webp'
+import Adjarian_Khachapuri from './menu/BAKING/Adjarian_Khachapuri.webp'
+import Cheese_Manakish from './menu/BAKING/Cheese_Manakish.webp'
+import Manakish_Zaatar from './menu/BAKING/Manakish_Zaatar.webp'
+import Lahm_bi_Ajeen from './menu/BAKING/Lahm_bi_Ajeen.webp'
+//Pizza
+import Pizza_4cheeses from './menu/PIZZA/Pizza_4cheeses.webp'
+import Pizza_4Meats from './menu/PIZZA/Pizza_4Meats.webp'
+import Salami from './menu/PIZZA/Salami.webp'
+import Margarita from './menu/PIZZA/Margarita.webp'
+//SANDWICHES
+import Falafel2 from './menu/SANDWICHES/Falafel.webp'
+import Kebab from './menu/SANDWICHES/Kebab.webp'
+import Shish_Tauk from './menu/SANDWICHES/Shish_Tauk.webp'
+import Shawarma from './menu/SANDWICHES/Shawarma.webp'
+
 
 
 
@@ -166,42 +190,42 @@ export const menu_list = [
     },{
         menu_name: "MAIN COURSE",
         menu_name_uk:"ОСНОВНА СТРАВА",
-        menu_image: Shakshuka_classic_cat,
+        menu_image: MAIN_COURSE_cat,
         addel: "Kichen"
     },{
         menu_name: "GRILL",
         menu_name_uk:"ГРИЛЬ",
-        menu_image: Shakshuka_classic_cat,
+        menu_image: Mixed_Meat_Shashlik_Platter_1000_cat,
         addel: "Kichen"
     },{
         menu_name: "HOT APPETIZERS",
         menu_name_uk:"ГАРЯЧІ ЗАКУСКИ",
-        menu_image: Shakshuka_classic_cat,
+        menu_image: HOT_APPETIZERS_cat,
         addel: "Kichen"
     },{
         menu_name: "BAKING",
         menu_name_uk:"ВИПІЧКА",
-        menu_image: Shakshuka_classic_cat,
+        menu_image: BAKING_cat,
         addel: "Kichen"
     },{
         menu_name: "PIZZA",
         menu_name_uk:"ПІЦА",
-        menu_image: Shakshuka_classic_cat,
+        menu_image: PIZZA_cat,
         addel: "Kichen"
     },{
         menu_name: "SANDWICHES",
         menu_name_uk:"СЕНДВІЧІ",
-        menu_image: Shakshuka_classic_cat,
+        menu_image: SANDWICHES_cat,
         addel: "Kichen"
     },{
         menu_name: "SIDES",
         menu_name_uk:"гарніри",
-        menu_image: Shakshuka_classic_cat,
+        menu_image: SIDES_cat,
         addel: "Kichen"
     },{
         menu_name: "DESSERTS",
         menu_name_uk:"ДЕСЕРТИ",
-        menu_image: Shakshuka_classic_cat,
+        menu_image: DESSERTS_cat,
         addel: "Kichen"
     }
 ]
@@ -761,7 +785,7 @@ export const food_list = [
         _id: "49",
         name:"Mixed Meat Shashlik Platter 0.5",
         name_uk: "Мікс Шашликів 0.5кг",
-        image: Mixed_Meat_Shashlik_Platter_500,
+        image: Mixed_Meat_Shashlik_Platter_1000,
         gram:500/250,
         price: 829,
         description: "Food provides essential nutrients for overall health and well-being",
@@ -783,7 +807,7 @@ export const food_list = [
         _id: "51",
         name:"Veal kebab",
         name_uk: "Кебаб телятини",
-        image: Veal_kebab,
+        image: Lamb_kebab,
         gram:200/100/75,
         price: 256,
         description: "Food provides essential nutrients for overall health and well-being",
@@ -816,7 +840,7 @@ export const food_list = [
         _id: "54",
         name:"Grilled Kebab Mix 0.5kg",
         name_uk: "Кебаб гриль мікс 0.5кг",
-        image: Grilled_Kebab_Mix_500,
+        image: Grilled_Kebab_Mix_1000,
         gram:500/250,
         price: 679,
         description: "Food provides essential nutrients for overall health and well-being",
@@ -849,7 +873,7 @@ export const food_list = [
         _id: "57",
         name:"Grilled Vegetables 0.150kg",
         name_uk: "Овочі гриль 0.150кг",
-        image: Grilled_Vegetables_150,
+        image: Grilled_Vegetables_300,
         gram:150,
         price: 123,
         description: "Food provides essential nutrients for overall health and well-being",
@@ -978,5 +1002,226 @@ export const food_list = [
         description_uk: "uk Food provides essential nutrients for overall health and well-being",
 
         category: "GRILL"
+    },{
+        _id: "68",
+        name:"Hummus with veal & Nuts",
+        name_uk: "Хумус з телятиною та горіхами",
+        image: Hummus_with_veal,
+        gram:200/80,
+        price: 292,
+        description: "Food provides essential nutrients for overall health and well-being",
+        description_uk: "uk Food provides essential nutrients for overall health and well-being",
+
+        category: "HOT APPETIZERS"
+    },{
+        _id: "68",
+        name:"Hummus with lamb",
+        name_uk: "Хумус з бараниною",
+        image: Hummus_with_veal,
+        gram:200/80,
+        price: 274,
+        description: "Food provides essential nutrients for overall health and well-being",
+        description_uk: "uk Food provides essential nutrients for overall health and well-being",
+
+        category: "HOT APPETIZERS"
+    },{
+        _id: "68",
+        name:"Falafel",
+        name_uk: "Фалафель",
+        image: Falafel,
+        gram:200/80,
+        price: 164,
+        description: "Food provides essential nutrients for overall health and well-being",
+        description_uk: "uk Food provides essential nutrients for overall health and well-being",
+
+        category: "HOT APPETIZERS"
+    },{
+        _id: "68",
+        name:"Fried Kebbe",
+        name_uk: "Кеббе смажене",
+        image: Fried_Kebbe,
+        gram:350/30,
+        price: 188,
+        description: "Food provides essential nutrients for overall health and well-being",
+        description_uk: "uk Food provides essential nutrients for overall health and well-being",
+
+        category: "HOT APPETIZERS"
+    },{
+        _id: "68",
+        name:"Chicken liver with onions",
+        name_uk: "Печінка куряча з цибулею",
+        image: Chicken_liver_with_onions,
+        gram:300,
+        price: 199,
+        description: "Food provides essential nutrients for overall health and well-being",
+        description_uk: "uk Food provides essential nutrients for overall health and well-being",
+
+        category: "HOT APPETIZERS"
+    },{
+        _id: "68",
+        name:"Chicken liver with sauce Narsharab",
+        name_uk: "Печінка куряча з соусом 'Наршараб'",
+        image: Chicken_liver_with_onions,
+        gram:300,
+        price: 209,
+        description: "Food provides essential nutrients for overall health and well-being",
+        description_uk: "uk Food provides essential nutrients for overall health and well-being",
+
+        category: "HOT APPETIZERS"
+    },{
+        _id: "69",
+        name:"Pidet with chicken, mushroom and cheese",
+        name_uk: "Піде з куркою, грибами та сиром",
+        image: Pidet_with_chicken,
+        gram:400,
+        price: 310,
+        description: "Food provides essential nutrients for overall health and well-being",
+        description_uk: "uk Food provides essential nutrients for overall health and well-being",
+
+        category: "BAKING"
+    },{
+        _id: "70",
+        name:"Pidet with meat and cheese",
+        name_uk: "Піде з м'ясом та сиром",
+        image: Pidet_with_meat_and_cheese,
+        gram:400,
+        price: 320,
+        description: "Food provides essential nutrients for overall health and well-being",
+        description_uk: "uk Food provides essential nutrients for overall health and well-being",
+
+        category: "BAKING"
+    },{
+        _id: "71",
+        name:"Adjarian Khachapuri",
+        name_uk: "Хачапурі по-аджарськи",
+        image: Adjarian_Khachapuri,
+        gram:400,
+        price: 320,
+        description: "Food provides essential nutrients for overall health and well-being",
+        description_uk: "uk Food provides essential nutrients for overall health and well-being",
+
+        category: "BAKING"
+    },{
+        _id: "72",
+        name:"Cheese Manakish",
+        name_uk: "Манакіш з сиром",
+        image: Cheese_Manakish,
+        gram:220,
+        price: 267,
+        description: "Food provides essential nutrients for overall health and well-being",
+        description_uk: "uk Food provides essential nutrients for overall health and well-being",
+
+        category: "BAKING"
+    },{
+        _id: "73",
+        name:"Manakish Zaatar",
+        name_uk: "Манакіш Заатар",
+        image: Manakish_Zaatar,
+        gram:220,
+        price: 267,
+        description: "Food provides essential nutrients for overall health and well-being",
+        description_uk: "uk Food provides essential nutrients for overall health and well-being",
+
+        category: "BAKING"
+    },{
+        _id: "74",
+        name:"Lahm bi Ajeen",
+        name_uk: "Лахме баоджин",
+        image: Lahm_bi_Ajeen,
+        gram:300,
+        price: 310,
+        description: "Food provides essential nutrients for overall health and well-being",
+        description_uk: "uk Food provides essential nutrients for overall health and well-being",
+
+        category: "BAKING"
+    },{
+        _id: "75",
+        name:"4 cheeses",
+        name_uk: "4 сира",
+        image: Pizza_4cheeses,
+        gram:500,
+        price: 410,
+        description: "Food provides essential nutrients for overall health and well-being",
+        description_uk: "uk Food provides essential nutrients for overall health and well-being",
+
+        category: "PIZZA"
+    },{
+        _id: "76",
+        name:"4 Meats",
+        name_uk: "4 М'яса",
+        image: Pizza_4Meats,
+        gram:500,
+        price: 410,
+        description: "Food provides essential nutrients for overall health and well-being",
+        description_uk: "uk Food provides essential nutrients for overall health and well-being",
+
+        category: "PIZZA"
+    },{
+        _id: "77",
+        name:"Salami",
+        name_uk: "Салямі",
+        image: Salami,
+        gram:500,
+        price: 390,
+        description: "Food provides essential nutrients for overall health and well-being",
+        description_uk: "uk Food provides essential nutrients for overall health and well-being",
+
+        category: "PIZZA"
+    },{
+        _id: "78",
+        name:"Margarita",
+        name_uk: "Маргарита",
+        image: Margarita,
+        gram:500,
+        price: 380,
+        description: "Food provides essential nutrients for overall health and well-being",
+        description_uk: "uk Food provides essential nutrients for overall health and well-being",
+
+        category: "PIZZA"
+    },{
+        _id: "79",
+        name:"Falafel",
+        name_uk: "Фалафель",
+        image: Falafel2,
+        gram:300/100,
+        price: 200,
+        description: "Food provides essential nutrients for overall health and well-being",
+        description_uk: "uk Food provides essential nutrients for overall health and well-being",
+
+        category: "SANDWICHES"
+    },{
+        _id: "80",
+        name:"Kebab",
+        name_uk: "Кебаб",
+        image: Kebab,
+        gram:300/100,
+        price: 200,
+        description: "Food provides essential nutrients for overall health and well-being",
+        description_uk: "uk Food provides essential nutrients for overall health and well-being",
+
+        category: "SANDWICHES"
+    },{
+        _id: "81",
+        name:"Shish Tauk",
+        name_uk: "Шиш-таук",
+        image: Shish_Tauk,
+        gram:300/100,
+        price: 200,
+        description: "Food provides essential nutrients for overall health and well-being",
+        description_uk: "uk Food provides essential nutrients for overall health and well-being",
+
+        category: "SANDWICHES"
+    },{
+        _id: "82",
+        name:"Shawarma",
+        name_uk: "Шаурма",
+        image: Shawarma,
+        gram:300/100,
+        price: 200,
+        description: "Food provides essential nutrients for overall health and well-being",
+        description_uk: "uk Food provides essential nutrients for overall health and well-being",
+
+        category: "SANDWICHES"
     },
+    
 ]
